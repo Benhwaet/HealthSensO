2851478d5ef3a00475547415aef4719efbcc57e7
BenHwaet <benmdesroches@gmail.com>
2023-10-14 14:31:34 -0300

    added avatar standin and changed font

--- client/src/components/pages/Header.js
+++ client/src/components/pages/Header.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Avatar from '@mui/material/Avatar';
+import avatar from '../../images/avatar.png';
 import './pages.css';
-import AvatarImg from '../circles/Avatar';
-const logo = require('../../images/Owhite.png');
+const logo = require('../../images/Owhite2.png');
 
 export default function Header() {
     return (
-        <header style={{ backgroundColor: 'black', height: '125px', width: '100%'}}>
+        <header style={{ backgroundColor: 'black', height: '150px', width: '100%'}}>
             <Container>
                 <Row>
                     <Col className="logout">
                         <a className="logoutText" href="Login">Log Out</a>
                         </Col>
-                    <Col style={{display: "flex", justifyContent: "center", alignItems: "center", position: "relative"}}>
-                    <img src={logo} alt="logo" style={{height: "125px"}}/>
-                    <AvatarImg/>
+                    <Col style={{display: "flex", marginTop: "15px", justifyContent: "center", alignItems: "center", position: "relative"}}>
+                    <img  className="top-logo" src={logo} alt="logo" style={{height: "125px"}}/>
+                    <Avatar className="avatar" src={avatar} alt="default avatar" />
                     </Col>
                     <Col className="help">
                         <a className="helpText" href="Help">Help</a>
---
+++ client/src/components/pages/section-nutrition/ServingCounter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+export default function ServingCounter() {
+    <Container>
+        <Row>
+            <Col> </Col>
+            <Col> </Col>
+            <Col> </Col>
+        </Row>
+    </Container>
+}
