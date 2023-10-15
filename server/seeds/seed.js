17546763097942f99262b551dba0d51646d68230
BenHwaet <benmdesroches@gmail.com>
2023-10-15 11:00:07 -0300

    trying to unf*ck branches

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,14 +1,8 @@
 const sequelize = require('../config/connection');
-const seedFood = require('./foodData');
-const seedPaintings = require('./paintingData');
 const seedUsers = require('./userData')
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-
-  await seedFood();
-
-  await seedPaintings();
 
   await seedUsers();
 
