cd211c4c25c1ccb66a5e8c071ef7b891beeb5c15
BenHwaet <benmdesroches@gmail.com>
2023-10-16 19:13:55 -0300

    routing working between pages, login form and register form appear as wanted

--- client/src/components/pages/Recipes/RecipeOC.js
+++ client/src/components/pages/Recipes/RecipeOC.js
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import {RecipeForm} from "./RecipeForm"
 import {RecipeOResult} from "./RecipeOResult"
-// import Header from "../Header.js"
-// import Footer from "../Footer.js"
 
 export default function RecipeOC(){
   const [results, setResults] = useState([]);
 
   return (
     <div>
-      {/* <Header></Header> */}
       <RecipeForm setResults={setResults}></RecipeForm>
       <RecipeOResult results={results}></RecipeOResult>
-      {/* <Footer></Footer> */}
     </div>
   );
 }
