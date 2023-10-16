ba07a2419858d366af4b22ea989a13fd4b47ec7c
jaden merzetti <jmerzet1@unb.ca>
2023-10-16 19:10:51 -0300

    Removed food and painting and added journal and logbook

--- server/models/index.js
+++ server/models/index.js
@@ -4,14 +4,6 @@ const Journal = require('./Journal');
 const Logbook = require('./Logbook');
 const Painting = require('./Painting');
 
-Food.hasMany(Painting, {
-  foreignKey: 'food_id',
-});
-
-Painting.belongsTo(Food, {
-  foreignKey: 'food_id',
-});
-
 Journal.belongsTo(User, {
   foreignKey: "user_id", 
 });
@@ -20,7 +12,4 @@ Logbook.belongsTo(User, {
   foreignKey: "user_id", 
 });
 
-module.exports = { User, Food, Painting };
-
-//user has many journals & jounral belongs to user
-// user has many logbooks & journal belongs to user  
+module.exports = { User, Journal, Logbook }; 
