4823b3a66048837743ae9b81fb9ed8ad6b474fe4
jaden merzetti <jmerzet1@unb.ca>
2023-10-16 19:13:52 -0300

    removed const variables

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,7 @@
 const User = require('./User');
-const Food = require('./Food');
 const Journal = require('./Journal');
 const Logbook = require('./Logbook');
-const Painting = require('./Painting');
+
 
 Journal.belongsTo(User, {
   foreignKey: "user_id", 
